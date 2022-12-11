d795174ea1717e49d7b89d9b348e823ffde8fbce
rajan4510 <rajankajal44@gmail.com>
2022-12-11 18:01:03 -0500

    jhbhjb

---
+++ testproject/src/testproject/testHoliday.java
@@ -0,0 +1,19 @@
+package testproject;
+
+public class testHoliday {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		Holiday holiday =  new Holiday(4, "Independence day", "july");
+         System.out.println(holiday.day);    
+         System.out.println(holiday.name);    
+         System.out.println(holiday.month);    
+//
+
+                  holiday.insamemonth();
+	}
+	
+
+}
